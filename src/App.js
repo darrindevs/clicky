8b82161ed324f136d192e451981ea1883f8a96b8
Zaino <hello@darrin.dev>
2021-05-19 07:54:57 -0700

    modify package.json with deploy scripts

--- src/App.js
+++ src/App.js
@@ -2,14 +2,38 @@ import React, { Component } from "react";
 import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
-//todo import the json objects 
+import robots from "./robots.json";
 
-//todo create the app class to extend the component and create state
 class App extends Component {
-  // do the work 
+  // Setting this.state.friends to the friends json array
+  state = {
+    robots: robots
   };
 
-//todo render the cards 
+  removeFriend = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const robots = this.state.robots.filter(robot => robot.id === id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ robots: robots });
+  };
 
-//✅ export App 
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Header>Robots!</Header>
+        {this.state.robots.map(robot => (
+          <Card
+            countRobot={this.countRobot}
+            id={robot.id}
+            key={robot.id}
+            name={robot.name}
+            image={robot.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
 export default App;
