a187e40232bc9390cf64f8d8a7ecfb717f6396b6
Zaino <hello@darrin.dev>
2021-05-18 17:20:56 -0700

    add components

---
+++ App.js
@@ -0,0 +1,13 @@
+//todo import React and set up components 
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+//todo import the json objects 
+
+//todo create the app class to extend the component and create state
+
+
+//todo render the cards 
+  
+//todo export App 
