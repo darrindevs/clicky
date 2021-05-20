45e3dab5f75a9e137aee7d5dc3f6992e58d1fa20
Zaino <hello@darrin.dev>
2021-05-20 09:50:25 -0700

    update header and styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,11 +5,15 @@ function Header(props) {
   return (
   <div className="header">
     <div className="title">{props.children}</div>
-      <div className="scores">
-          <span className="score">Score: {props.score}</span>
-          <span className="highscore">Highscore: {props.highscore}</span>
+      
+      <div className="scores pt-3">
+          <span className="small me-5 fst-italic">Only click each robot once!</span>
+          <span className="score me-3">Score: {props.score}</span>
+          <span className="highscore me-3">Highscore: {props.highscore}</span>
       </div>
-    </div>
+      
+      
+  </div>
   );
 }
  
