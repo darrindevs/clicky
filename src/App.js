ffaf8d8490e8d934723b5f69d382d14d6717720a
Zaino <hello@darrin.dev>
2021-05-20 09:24:11 -0700

    add functioning game

--- src/App.js
+++ src/App.js
@@ -5,17 +5,34 @@ import Header from "./components/Header";
 import robots from "./robots.json";
 
 class App extends Component {
-//todo track the state of clicked cards
+//✅ todo track the state of clicked cards
 state = {
   robots,
   score: 0,
-  highscore: 0
+  highscore: 0, 
+  clicked: []
+
 };
 
-//✅todo increment the score when cards clicked
-clickCount = () => {
-  //use the setState method to update a component's state
-  this.setState({ score: this.state.score + 1 });
+//✅ todo increment the score when cards clicked
+clickCount = (id) => {
+  //if the clicked id already exists reset the clicked array and score to 0 
+  if (this.state.clicked.indexOf(id) !== -1) {
+    this.setState(
+      { clicked: [],
+        score: 0
+      }
+    );
+    //and alert user game over 
+    alert(`Oops! Game over! Your score: ${this.state.score}`);
+  } else {
+  //otherwise keep track of newly clicked in the state and increment 
+  var newClicked = this.state.clicked;
+  newClicked.push(id);//add the newly clicked elements to the array 
+  this.setState({ clicked: newClicked });// and reset the state of clicked to include newClicked 
+  this.setState({ score: this.state.score + 1 });// increment the score 
+  this.state.robots.sort(() => Math.random() - 0.5); //shuffle the robots! 
+  }
 };
 
 //todo display the score + high score in the header 
